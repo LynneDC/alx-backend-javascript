1d1efb5eab6121b8fa350cb08fc32ef21c5c56b1
LynneDC <roselinedc1308@gmail.com>
2024-01-03 09:04:56 +0000

    remove comments tp fix checker

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,3 @@
-/*
- * modify
- * function taskFirst to instantiate variables using const
- * function taskNext to instantiate variables using let
- */
-
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
