fb945553de7e45235e9d8cf7a98ea5471c86ffd4
LynneDC <roselinedc1308@gmail.com>
2024-01-03 07:56:37 +0000

    fix to lin variable rep

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,15 @@
 /*
- * uses hoisting principles 
+ * uses hoisting principles
  * and satsfiy lint requirements
  * on changing var to const
  */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
