865b474a8a0f336211f4ea59cb78330070bcb669
LynneDC <roselinedc1308@gmail.com>
2024-01-08 10:37:23 +0000

    call super in extend method

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,30 @@
+/**
+ * import class Building
+ * implement child class that extends from building
+ * give it 2 params in constructor
+ * each attribute must have _param
+ * implement getter
+ * override evacuation Warning Message method
+ * return a string
+ */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // getter method
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return (`vacuate slowly the ${this.floors} floors`);
+  }
+}
