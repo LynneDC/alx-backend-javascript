03cd172ec9ef2764ae7c86ac80ec491ff35987b7
LynneDC <roselinedc1308@gmail.com>
2024-01-10 07:49:26 +0000

    fix error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,18 @@
  * @startString values to be returned
  */
 export default function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.join('-').replace(new RegExp(`^${startString}`), '');
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
