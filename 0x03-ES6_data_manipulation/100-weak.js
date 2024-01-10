34fc421e7a42e04e9c0305260b7abaa2945c45d5
LynneDC <roselinedc1308@gmail.com>
2024-01-10 07:38:43 +0000

    weak link

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count = +count;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  console.log(count);
+}
