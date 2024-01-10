2d7dcb3d79f3e2dcb2d6d1bf11a4bd9e63f8896a
LynneDC <roselinedc1308@gmail.com>
2024-01-10 07:48:21 +0000

    fix error

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
-  let count = weakMap.get(endpoint) || 0;
-  count = +count;
-  weakMap.set(endpoint, count);
+const MAX_ENDPOINT_CALLS = 5;
 
-  if (count >= 5) {
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
-
-  console.log(count);
 }
