7048ea12d8bebecddf7e6cf6a6d27aeb0ce0b5b1
LynneDC <roselinedc1308@gmail.com>
2024-01-10 06:29:42 +0000

    uses reduce to calculate the total of student id

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -3,5 +3,5 @@
  * accept list of student from prev fun
  */
 export default function getStudentIdsSum(arg) {
-	return arg.reduce((accumulator, student) => accumulator + student.id, 0);
+  return arg.reduce((accumulator, student) => accumulator + student.id, 0);
 }
