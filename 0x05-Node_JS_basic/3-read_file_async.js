80c481ebcb95edc93f1c643cb5eeefdcfd218f72
LynneDC <roselinedc1308@gmail.com>
2024-02-26 18:29:19 +0200

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,34 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const students = data
-          .split('\n')
-          .filter((student) => student.length > 0)
-          .map((student) => student.split(','));
+	    return new Promise((resolve, reject) => {
+		            fs.readFile(path, 'utf8', (error, data) => {
+				                if (error) {
+							                reject(new Error('Cannot load the database'));
+							            } else {
+									                    const lines = data.trim().split('\n');
+									                    const students = {};
 
-        const nbStudents = students.length;
-        console.log(`Number of students: ${nbStudents}`);
+									                    lines.forEach(line => {
+												                        const [firstName, lastName, age, field] = line.split(',');
+												                        if (firstName && lastName && age && field) {
+																                        if (!students[field]) {
+																				                            students[field] = [];
+																				                        }
+																                        students[field].push(firstName);
+																                    }
+												                    });
 
-        const fields = ['firstName', 'lastName', 'email', 'studentId', 'school'];
-        fields.forEach((field) => {
-          const nbStudentsInField = students.reduce((acc, curr) => {
-            if (curr[0] === field) {
-              return acc + 1;
-            }
-            return acc;
-          }, 0);
-          const listOfFirstNames = students
-            .filter((student) => student[0] === field)
-            .map((student) => student[1]);
-          console.log(`Number of students in ${field}: ${nbStudentsInField}`);
-          console.log(`  List: ${listOfFirstNames.join(', ')}`);
-        });
+									                    console.log(`Number of students: ${lines.length}`);
+									                    for (const field in students) {
+												                        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+												                    }
 
-        resolve();
-      }
-    });
-  });
+									                    resolve();
+									                }
+				            });
+		        });
 }
+
+module.exports = countStudents;
+
